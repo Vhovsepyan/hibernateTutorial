3a7978b016291cdabf359dfa6e8ad60cc4bceb00
Vahe <vahe.h.x@gmail.com>
2018-09-14 15:54:36 +0400

    hibernate CRUD

---
+++ src/com/home/demo/ReadEmployeeDemo.java
@@ -0,0 +1,7 @@
+package com.home.demo;
+
+/**
+ * Created by vahe on 9/14/18.
+ */
+public class ReadEmployeeDemo {
+}
---
+++ src/com/home/demo/model/Employee.java
@@ -0,0 +1,7 @@
+package com.home.demo.model;
+
+/**
+ * Created by vahe on 9/14/18.
+ */
+public class Empolyee {
+}
